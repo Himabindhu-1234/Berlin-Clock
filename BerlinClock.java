abebb18e4b2f22db0ee37ddb42985cab35e8c4ca
Himabindhu-1234 <84571787+Himabindhu-1234@users.noreply.github.com>
2021-05-22 19:11:46 +0530

    Add files via upload

---
+++ BerlinClock.java
@@ -0,0 +1,51 @@
+package JavaPrograms;
+
+import java.util.stream.IntStream;
+
+public class BerlinClock {
+    public String berlinClock(int hours, int minutes, int seconds) {
+        return getSeconds(seconds) + " " + getHours(hours) + " " + getMinutes(minutes);
+    }
+
+    protected String getSeconds(int seconds) {
+        return seconds % 2 == 0 ? "Y" : "O";
+    }
+
+    protected String getHours(int hours) {
+        int numberTopHourLamps = hours / 5;
+        int numberBottomHourLamps = hours % 5;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(getLampRow(4, numberTopHourLamps, "R"))
+                .append(" ")
+                .append(getLampRow(4, numberBottomHourLamps, "R"));
+
+        return sb.toString();
+    }
+
+    protected String getMinutes(int minutes) {
+        int numberTopMinutesLamps = minutes / 5;
+        int numberBottomMinutesLamps = minutes % 5;
+
+        StringBuilder sb = new StringBuilder();
+        IntStream.rangeClosed(1, 11)
+                .forEach(i -> sb.append(i <= numberTopMinutesLamps ? getMinuteLampColour(i) : "O"));
+
+        sb.append(" ");
+
+        sb.append(getLampRow(4, numberBottomMinutesLamps, "Y"));
+
+        return sb.toString();
+    }
+
+    private String getLampRow(int totalNumberLamps, int numberLampsOn, String lampSymbol) {
+        StringBuilder sb = new StringBuilder();
+        IntStream.rangeClosed(1, totalNumberLamps)
+                .forEach(i -> sb.append(i <= numberLampsOn ? lampSymbol : "O"));
+        return sb.toString();
+    }
+
+    private String getMinuteLampColour(int index) {
+        return index % 3 == 0 ? "R" : "Y";
+    }
+}
